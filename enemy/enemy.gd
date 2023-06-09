5bbf82ed8b73a83ce785d6e303576371550e6ff0
Kirschzelle <121607089+Kirschzelle@users.noreply.github.com>
2023-06-09 13:51:20 +0200

    feat: detection range in enemy
    moved all detection capabilities to enemy class, so you can easily define the area 2d with just the detectionRange parameter. Everything else is handled automatically.

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,10 +1,41 @@
 extends CharacterBody2D
 
+#init in specific enemy script
 var attack
 var weight
 var attackSpeed
+var attackCooldown
 var armor
-var armorAmount
+var detectionRange
+
+#use in specific enemy script
 var inRange = false
 @onready
 var player = $/root/parent/player
+
+#internal vars
+var collisionShape
+var detectionArea
+var circle
+
+func _ready():
+	circle.radius = detectionRange
+	detectionArea.monitorable = false
+	detectionArea.body_entered.connect(_player_entered_detection_area,player.get_instance_id())
+	detectionArea.body_exited.connect(_player_exited_detection_area,player.get_instance_id())
+
+func _init():
+	detectionArea = Area2D.new()
+	add_child(detectionArea)
+	collisionShape = CollisionShape2D.new()
+	circle = CircleShape2D.new()
+	collisionShape.shape = circle
+	detectionArea.collision_mask = CollisionLayers.COLLISION_LAYER_PLAYER
+	detectionArea.collision_layer =  0
+	detectionArea.add_child(collisionShape)
+
+func _player_entered_detection_area(_body):
+	inRange = true
+	
+func _player_exited_detection_area(_body):
+	inRange = false

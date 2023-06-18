09cb23488f5663de3e41d0ff665535e7f24d3b15
Kirschzelle <nikolassc@edu.aau.at>
2023-06-18 18:38:58 +0200

    feat: shadows
    added shadows

---
+++ level/pointilighty.gd
@@ -0,0 +1,11 @@
+extends PointLight2D
+const ENERGYLEVEL = 0.4
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	energy = ENERGYLEVEL
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
